b7f454f199bf50c25e17adc07500806555e2ff03
Julio Dreher <julio.s.dreher@gmail.com>
2023-02-22 11:29:44 -0300

    Project Created

---
+++ Hooks/orgsHooks/App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+function App(){
+
+  return <SafeAreaView>
+    <Text>Bem Vindo</Text>
+  </SafeAreaView>
+  
+}
+
+export default App;
